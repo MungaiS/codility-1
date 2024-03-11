8ff4831bcfa6cabb2c8edae955ea939eaf271add
MungaiS <129229232+MungaiS@users.noreply.github.com>
2024-03-11 16:10:23 +0300

    Started challenge

---
+++ solution.py
@@ -0,0 +1,17 @@
+def Solution(A, D):
+    balance = 0
+    fees = 0
+    card_payments = 0
+    card_payments_amount = 0
+    for i in range(len(A)):
+        if A[i] < 0:
+            card_payments += 1
+            card_payments_amount += i
+        else:
+            balance += A[i]
+        date = D[i].split("-")
+        if 
+        pass
+            if 
+
+Solution([1,-1,0,-105,1],["2020-12-31","2020-04-04","2020-04-14","2020-07-12"])
