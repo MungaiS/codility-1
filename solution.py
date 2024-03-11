ec3cf54eca38fa3daafc003c92d24f25afce7c65
MungaiS <129229232+MungaiS@users.noreply.github.com>
2024-03-11 16:44:05 +0300

    Done with codility

--- solution.py
+++ solution.py
@@ -1,17 +1,30 @@
 def Solution(A, D):
+# define variables for items we will need in our code
     balance = 0
     fees = 0
     card_payments = 0
     card_payments_amount = 0
+    date = D[i].split("-")
+
+  # Iterate through the list A to find trasanctions less than 0 and add the to the card payments count.
     for i in range(len(A)):
         if A[i] < 0:
             card_payments += 1
-            card_payments_amount += i
+            card_payments_amount += A[i]
         else:
             balance += A[i]
-        date = D[i].split("-")
-        if 
-        pass
-            if 
+            if card_payments >3 and card_payment_amount >= -100:
+                fees += 5
+                card_payments = 0
+                card_payment_amount = 0
+            else:
+                fees += 5
+                card_payments = 0
+                card_payment_amount = 0
+                balance -= 5
+    if card_payments >= 3 and card_payment_amount >= -100:
+        fees += 5
+    return balance - fees
+  
 
-Solution([1,-1,0,-105,1],["2020-12-31","2020-04-04","2020-04-14","2020-07-12"])
+print(Solution([1,-1,0,-105,1],["2020-12-31","2020-04-04","2020-04-14","2020-07-12"]))
